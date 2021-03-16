31d52b028a97fa58ba381de88c66de1d05f21ca2
Brian Longmore <brianl.ext@gmail.com>
2021-03-15 17:18:14 -1000

    added huge mandelbrot

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ var (
 func main() {
 	options := util.OutputConfig{
 		Src:          "https://www.researchgate.net/profile/John-Hubbard-6/publication/228540477/figure/fig1/AS:669410264088623@1536611252979/The-Mandelbrot-set-M-The-two-main-conjectures-are-the-following-MLC-The-set-M-is.png",
-		Dst:          "./prints/mandelbrotSmall.txt",
+		Dst:          "./prints/mandelbrotHuge.txt",
 		OutputMode:   "ascii",
 		AsciiPattern: " .:-=+*#%@",
-		OutputWidth:  200,
+		OutputWidth:  2000,
 		IsUseWeb:     true,
 		IsInverted:   true,
 		IsPrinted:    false,
