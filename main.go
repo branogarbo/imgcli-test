2c5594842230570bdac51ca502a8af8afa7d8051
Brian Longmore <brianl.ext@gmail.com>
2021-03-15 16:45:11 -1000

    added more mandelbrot prints

--- main.go
+++ main.go
@@ -15,15 +15,15 @@ var (
 func main() {
 	options := util.OutputConfig{
 		Src:          "https://www.researchgate.net/profile/John-Hubbard-6/publication/228540477/figure/fig1/AS:669410264088623@1536611252979/The-Mandelbrot-set-M-The-two-main-conjectures-are-the-following-MLC-The-set-M-is.png",
-		Dst:          "./prints/mandelbrot.txt",
+		Dst:          "./prints/mandelbrotSmall.txt",
 		OutputMode:   "ascii",
 		AsciiPattern: " .:-=+*#%@",
 		OutputWidth:  200,
 		IsUseWeb:     true,
 		IsInverted:   true,
-		IsPrinted:    true,
-		IsSaved:      false,
-		IsQuiet:      true,
+		IsPrinted:    false,
+		IsSaved:      true,
+		IsQuiet:      false,
 	}
 
 	pixelString, err = util.OutputImage(options)
