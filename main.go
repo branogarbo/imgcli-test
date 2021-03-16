debe057ebfeda39d6e751f37c6af8cd6d1a6c764
Brian Longmore <brianl.ext@gmail.com>
2021-03-15 15:19:00 -1000

    added examples

--- main.go
+++ main.go
@@ -14,15 +14,15 @@ var (
 
 func main() {
 	options := util.OutputConfig{
-		Src:          "./images/portrait.jpg",
-		Dst:          "./prints/portrait.txt",
+		Src:          "https://www.researchgate.net/profile/John-Hubbard-6/publication/228540477/figure/fig1/AS:669410264088623@1536611252979/The-Mandelbrot-set-M-The-two-main-conjectures-are-the-following-MLC-The-set-M-is.png",
+		Dst:          "./prints/mandelbrot.txt",
 		OutputMode:   "ascii",
 		AsciiPattern: " .:-=+*#%@",
-		OutputWidth:  150,
-		IsUseWeb:     false,
-		IsInverted:   false,
-		IsPrinted:    false,
-		IsSaved:      true,
+		OutputWidth:  200,
+		IsUseWeb:     true,
+		IsInverted:   true,
+		IsPrinted:    true,
+		IsSaved:      false,
 		IsQuiet:      true,
 	}
 
@@ -32,5 +32,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Println(pixelString)
+//	fmt.Println(pixelString)
 }
